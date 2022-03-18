e0ba90bb658a498afacf6f83e0d2b85d963961cb
Shafiullah Adid <99551580+shafiullahadid10@users.noreply.github.com>
2022-03-18 23:11:24 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// project testimonial slider code
+
+$('.owl-carousel').owlCarousel({
+		    loop:true,
+		    margin:10,
+		    nav:true,
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        780:{
+		            items:1
+		        },
+		        1000:{
+		            items:3
+		        }
+		    }
+		})
